2f321ae97924fb12c162c452a2ba6df7106677b4
Steven Paalz <stevenpaalz@gmail.com>
2023-02-02 14:36:04 -0500

    my transpose

--- lib/tdd.rb
+++ lib/tdd.rb
@@ -8,4 +8,26 @@ class Array
         return hash.keys
     end
     
+    def two_sum
+        result = []
+        (0...self.length - 1).each do |i|
+           (i+1...self.length).each do |j|
+                result << [i,j] if self[i] + self[j] == 0
+            end
+        end
+        result
+    end
+
+end
+
+def my_transpose(array)
+    result = []
+    (0...array.length).each do |row|
+        sub = []
+        (0...array.length).each do |col|
+            sub << array[col][row]
+        end
+        result << sub
+    end
+    result
 end
--- spec/tdd_spec.rb
+++ spec/tdd_spec.rb
@@ -2,6 +2,7 @@ require 'rspec'
 require 'tdd'
 
 describe Array do
+
     describe '#my_uniq' do 
         it "should return unqiue elements" do 
             expect([1,2,1,3,3].my_uniq).to eq([1,2,3])
@@ -11,5 +12,30 @@ describe Array do
             array2 = array1.my_uniq
             expect(array1).to_not be(array2)
         end
+    end
+
+    describe '#two_sum' do 
+        it "each set of indices should sum to 0" do
+            expect([-1,0,2,-2,1].two_sum).to eq([[0,4],[2,3]])
+        end
+    end
+
+end
+
+describe 'my_transpose' do
+    it "should transpose the 2D grid" do
+        grid1 = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8]
+          ]
+
+        grid2 = [
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8]
+        ]
+
+        expect(my_transpose(grid1)).to eq(grid2)
     end
 end
